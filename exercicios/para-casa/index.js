a1e5bdb34890131655993f7acbbd8cda2ac8684c
GRACE KELLY <grace.limafe@gmail.com>
2022-09-29 17:22:55 -0300

    Utilizando DOM para criar filtros

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,69 @@
+var btn = document.querySelector('#btn');
+var container = document.querySelector('.container');
+
+
+btn.addEventListener("click", function() {
+    
+    if(container.style.display === 'block')
+    {
+        container.style.display = 'none';
+    } 
+    else 
+    {
+        container.style.display = 'block';
+    }
+
+    
+
+});
+
+var paragrafo = document.querySelector('#paragrafo');
+var container1 = document.querySelector('.container1');
+
+
+paragrafo.addEventListener("click", function() {
+    
+    if(container1.style.display === 'block')
+    {
+        container1.style.display = 'none';
+    } 
+    else 
+    {
+        container1.style.display = 'block';
+    }
+
+    
+
+});
+
+var text = document.querySelector('#text');
+var container2 = document.querySelector('.container2');
+
+
+text.addEventListener("click", function() {
+    
+    if(container2.style.display === 'block')
+    {
+        container2.style.display = 'none';
+    } 
+    else 
+    {
+        container2.style.display = 'block';
+    }
+
+    
+
+});
+
+
+
+
+
+
+
+
+    
+
+
+
+
