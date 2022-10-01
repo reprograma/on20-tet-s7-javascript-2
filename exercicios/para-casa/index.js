03bc4b5dfd88599b3577499c4a6863d75e59d2e5
daiane-dosanjos <dayanjos01@gmail.com>
2022-10-01 02:22:53 -0300

    Criando programa para o usuário filtrar os produtos desejado, atividade semana 7, Daiane Anjos

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,101 @@
+let produtos = [
+	{
+	  nome: "TV 20 polegadas",
+	  categoria: "áudio e vídeo",
+	  preco: 1500
+	},
+	{
+	  nome: "Mouse",
+	  categoria: "informática",
+	  preco: 70
+	},
+	{
+	  nome: "Teclado",
+	  categoria: "informática",
+	  preco: 350 
+	},
+	{
+	  nome: "Aparelho de som",
+	  categoria: "áudio e vídeo",
+	  preco: 600
+	},
+	{
+	  nome: "Sanduicheira",
+	  categoria: "eletrodomésticos",
+	  preco: 130
+	},
+	{
+	  nome: "Microondas 20L",
+	  categoria: "eletrodomésticos",
+	  preco: 800
+	},
+	{
+	  nome: "Geladeira frost free",
+	  categoria: "eletrodomésticos",
+	  preco: 2100
+	},
+	{
+	  nome: "Carregador de notebook Dell",
+	  categoria: "informática",
+	  preco: 140
+	},
+	{
+	  nome: "Caixa de som JBL",
+	  categoria: "áudio e vídeo",
+	  preco: 230
+	},
+	{
+	  nome: "Webcam",
+	  categoria: "informática",
+	  preco: 330
+	},
+	{
+	  nome: "Fogão 4 bocas",
+	  categoria: "eletrodomésticos",
+	  preco: 680
+	},
+	{
+	  nome: "Monitor 24 polegadas",
+	  categoria: "informática",
+	  preco: 900
+	},
+	{
+	  nome: "Air fryer",
+	  categoria: "eletrodomésticos",
+	  preco: 450
+	}
+  ]
+
+  
+  let botoesCategoria = document.querySelectorAll('button');
+
+  function listarProdutos(listaProdutos) {
+	let section = document.querySelector('section')
+	let listaExistente = document.querySelectorAll('.produto')
+  
+	if (listaExistente.length > 0) {
+	  listaExistente.forEach(function (itemListaExistente) {
+		section.removeChild(itemListaExistente);
+	  })
+	}  
+	
+	listaProdutos.forEach(function (produtoASerListado) {
+	  let p = document.createElement('p');
+	  p.classList.add('produto')
+	  p.innerText = `Produto: ${produtoASerListado.nome} \n Preço: ${produtoASerListado.preco}`;
+	  section.appendChild(p);
+	})
+  }
+
+  function filtrarProdutos(e) {
+	let produtosFiltrados = [];
+	produtosFiltrados = produtos.filter(function (produto) {
+	  return produto.categoria === e.target.innerText;
+	});
+	listarProdutos(produtosFiltrados);
+  }
+
+    
+  for (let botao of botoesCategoria) {
+	botao.addEventListener('click', filtrarProdutos);
+  }
